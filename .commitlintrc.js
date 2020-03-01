7356a4587a75f92916f787324f6cc2ca2d7edad2
Mark <mark@remarkablemark.org>
2020-03-01 17:13:15 -0500

    chore(root): update commitlintrc and add "scope-enum" rule
    scope-enum values:
    
    * all
    * root
    * directory names within packages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,13 @@
+const { readdirSync } = require('fs');
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-enum': [
+      2,
+      'always',
+      ['all', require('./package').name, ...readdirSync('./packages')],
+    ],
+    'scope-empty': [2, 'never'],
+  },
 };
