0cdc5eb5996d3d6bb4d796360cfeb1cec7b5d2eb
Mark <mark@remarkablemark.org>
2020-06-21 17:19:12 -0400

    docs(web-scripts-migration): update JSDoc and add @see links

--- packages/web-scripts-migration/utilities.js
+++ packages/web-scripts-migration/utilities.js
@@ -5,7 +5,9 @@ const { writeFileSync } = require('fs');
 const cwd = process.cwd();
 
 /**
- * Runs command.
+ * Runs command with `exec`.
+ *
+ * @see {@link https://nodejs.org/api/child_process.html#child_process_child_process_execsync_command_options}
  *
  * @param {String} command
  * @return {String}
@@ -17,7 +19,9 @@ const exec = command =>
   });
 
 /**
- * Logs to console.
+ * Prints to `stdout` with newline.
+ *
+ * @see {@link https://nodejs.org/api/console.html#console_console_log_data_args}
  *
  * @param {...*} args
  */
@@ -25,6 +29,8 @@ const log = (...args) => console.log('INFO:', ...args);
 
 /**
  * Writes to file.
+ *
+ * @see {@link https://nodejs.org/api/fs.html#fs_fs_writefilesync_file_data_options}
  *
  * @param {String} file
  * @param {*} data
