5e3a10e318ed9b19677215488fc6b804c9270f92
Mark <mark@remarkablemark.org>
2020-06-28 14:33:47 -0400

    test(web-scripts-migration): validate `require-default` transform
    - package.json
      - Save `jest` to devDependencies
      - Include `/transforms/require-default.js` in "files"
    - transforms
      - Add test fixtures for `require-default`

---
+++ packages/web-scripts-migration/transforms/__testfixtures__/require-default.output.js
@@ -0,0 +1,11 @@
+require('a');
+
+let b = require('./b');
+
+const { c } = require('./c');
+
+require('./d.png').default;
+
+var e = require('./e.jpeg').default;
+
+const f = require('./f.mp3').default;
